c5033dcaffce2c3c8c3811b603baa4040756c656
kaycodes13 <egad.coding@gmail.com>
2025-12-31 00:12:20 -0500

    break hud function into separate class

---
+++ Mechanics/HUD.cs
@@ -0,0 +1,178 @@
+﻿using GlobalEnums;
+using Needleforge.Data;
+using Silksong.UnityHelper.Extensions;
+using System.Collections;
+using TravellerCrest.Components;
+using TravellerCrest.Data;
+using UnityEngine;
+using UnityEngine.Rendering;
+using static TravellerCrest.Utils.AssetUtil;
+using static UnityEngine.ParticleSystem;
+
+namespace TravellerCrest.Mechanics;
+
+internal static class HUD {
+
+	static HudFrameData Hud => SifCrest.HudFrame;
+
+		const string path = $"{nameof(TravellerCrest)}.Assets.Sprites";
+
+	internal static void Setup() {
+		tk2dSpriteAnimation
+			mainLib = AnimationManager.MainLib,
+			steelLib = AnimationManager.libraries["steel"];
+
+		Hud.Appear = mainLib.GetClipByName("Traveller HUD Appear");
+		Hud.Idle = mainLib.GetClipByName("Traveller HUD Idle");
+		Hud.Disappear = mainLib.GetClipByName("Traveller HUD Disappear");
+
+		Hud.SteelAppear = steelLib.GetClipByName("Traveller HUD Appear");
+		Hud.SteelIdle = steelLib.GetClipByName("Traveller HUD Idle");
+		Hud.SteelDisappear = steelLib.GetClipByName("Traveller HUD Disappear");
+
+
+		Hud.ProfileIcon = LoadSprite($"{path}.hud_profile.png", ppu: 100);
+		Hud.SteelProfileIcon = LoadSprite($"{path}.hud_ss_profile.png", ppu: 100);
+
+		Hud.OnRootCreated += BuildRoot;
+		Hud.Coroutine = MeterCoro;
+	}
+
+	private static void BuildRoot() {
+		GameObject root = Hud.Root!;
+		AddSortingGroup(root, 0);
+		root.transform.localPosition = new(-1.54f, 0.15f, 0);
+
+		var (meterObj, meterRend)
+			= NewHudElement<SpriteRenderer>("meter", order: 0);
+		meterRend.sprite = LoadSprite($"{path}.hud_meter.png");
+		meterRend.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+
+		var (maskObj, maskRend)
+			= NewHudElement<SpriteMask>("mask", order: 1);
+		maskRend.sprite = LoadSprite($"{path}.hud_meter_mask.png");
+		maskObj.transform.localScale = Vector3.zero;
+
+		var (burstGo, _)
+			= NewHudElement<MeshRenderer>("burst", order: 10);
+		burstGo.SetActive(false);
+		var burstDeac = burstGo.AddComponent<DeactivateAfter2dtkAnimation>();
+		burstDeac.animators = [burstGo.AddComponent<tk2dSpriteAnimator>()];
+		burstGo.AddComponent<tk2dSprite>();
+		burstGo.transform.localScale = 0.6f * Vector3.one;
+
+		HeroController.instance.InvokeNextFrame(FixPositionAndLayering);
+	}
+
+	private static IEnumerator MeterCoro(BindOrbHudFrame hudInstance) {
+		PlayerData pd = PlayerData.instance;
+		GameObject
+			mask = Hud.Root!.FindChild("mask")!,
+			burst = Hud.Root!.FindChild("burst")!;
+
+		var burstanim = burst.GetOrAddComponent<tk2dSpriteAnimator>();
+		burstanim.library = hudInstance.animator.library;
+		burstanim.DefaultClipId = hudInstance.animator.library.GetClipIdByName("Soul Burst Q");
+
+		while (true) {
+			if (HeroController.instance.IsPaused()) {
+				yield return null;
+				continue;
+			}
+			int masksMissing = pd.maxHealth - pd.health;
+			if (masksMissing == 9) {
+				if (mask.transform.localScale != Vector3.one) {
+					burst.SetActive(false);
+					burst.SetActive(true);
+					hudInstance.wandererHarpAppearAudio.SpawnAndPlayOneShot(
+						GlobalSettings.Audio.DefaultUIAudioSourcePrefab,
+						hudInstance.transform.position
+					);
+				}
+				mask.transform.localScale = Vector3.one;
+			}
+			else {
+				mask.transform.localScale = (masksMissing / 10f) * Vector3.one;
+			}
+			yield return null;
+		}
+	}
+
+	#region Local Utilities
+
+	private static SortingGroup AddSortingGroup(GameObject go, int order) {
+		var group = go.AddComponent<SortingGroup>();
+		group.sortingLayerName = "Over";
+		group.sortingOrder = order;
+		return group;
+	}
+
+	private static GameObject NewSortingGroup(
+		string name, int order, GameObject? group = null
+	) {
+		GameObject elt = new(name);
+
+		if (group)
+			elt.transform.SetParent(group!.transform);
+		else
+			elt.transform.SetParent(Hud.Root!.transform);
+
+		elt.transform.localScale = Vector3.one;
+		elt.transform.localPosition = Vector3.zero;
+		elt.layer = (int)PhysLayers.UI;
+
+		AddSortingGroup(elt, order);
+
+		return elt;
+	}
+
+	private static (GameObject, T) NewHudElement<T>(
+		string name, int order, GameObject? group = null
+	) where T : Renderer {
+		GameObject elt = new(name);
+
+		if (group)
+			elt.transform.SetParent(group!.transform);
+		else
+			elt.transform.SetParent(Hud.Root!.transform);
+
+		elt.transform.localScale = Vector3.one;
+		elt.transform.localPosition = Vector3.zero;
+		elt.layer = (int)PhysLayers.UI;
+
+		T renderer = elt.AddComponent<T>();
+		renderer.sortingLayerName = "Over";
+		renderer.sortingOrder = order;
+
+		return (elt, renderer);
+	}
+
+	/// <summary>
+	/// Sets the scale and coordinates of the hud root and hud root container to 
+	/// consistent values that are easy to work with. I really should be fixing these
+	/// things in Needleforge, not here, but that's a task for another day.
+	/// </summary>
+	private static void FixPositionAndLayering() {
+		Transform
+			root = Hud.Root!.transform,
+			rootParent = root.parent,
+			hudFrame = rootParent.parent;
+
+		rootParent.localScale = Vector3.one;
+		rootParent.localPosition = Vector3.zero;
+
+		var activator = rootParent.gameObject.GetOrAddComponent<HudRootActivator>();
+		activator.hudroot = root.gameObject;
+		activator.crest = SifCrest;
+
+		root.localScale = Vector3.one;
+
+		// critical for layering the hud frame, meter, and full-silk-orb correctly
+		float bindOrbZ = hudFrame.position.z;
+		Vector3 rootPos = root.position;
+		root.position = new(rootPos.x, rootPos.y, bindOrbZ - 0.00001f);
+	}
+
+	#endregion
+
+}
--- TravellerCrestPlugin.cs
+++ TravellerCrestPlugin.cs
@@ -1,18 +1,12 @@
 using BepInEx;
 using BepInEx.Logging;
-using GlobalEnums;
 using HarmonyLib;
 using Needleforge;
 using Needleforge.Data;
-using Silksong.UnityHelper.Extensions;
-using System.Collections;
 using System.Reflection;
 using TeamCherry.Localization;
-using TravellerCrest.Components;
-using TravellerCrest.Data;
 using TravellerCrest.Mechanics;
 using UnityEngine;
-using UnityEngine.Rendering;
 using static TravellerCrest.Utils.AssetUtil;
 
 namespace TravellerCrest;
@@ -62,9 +56,6 @@ public partial class TravellerCrestPlugin : BaseUnityPlugin {
 
 		Harmony.PatchAll();
 
-		Assembly asm = Assembly.GetExecutingAssembly();
-		const string path = $"{nameof(TravellerCrest)}.Assets.Sprites";
-
 		#region Custom Tool Colour
 
 		Purple.AddValidTypes(ToolItemType.Red, ToolItemType.Blue);
@@ -73,6 +64,8 @@ public partial class TravellerCrestPlugin : BaseUnityPlugin {
 		#endregion
 
 		#region Tool Slots
+
+		const string path = $"{nameof(TravellerCrest)}.Assets.Sprites";
 
 		SifCrest.RealSprite = LoadSprite($"{path}.crest_lines.png", ppu: 100);
 		SifCrest.Silhouette = LoadSprite($"{path}.crest_silhouette.png", ppu: 200);
@@ -91,102 +84,9 @@ public partial class TravellerCrestPlugin : BaseUnityPlugin {
 
 		#endregion
 
-		#region HUD
-
-		tk2dSpriteAnimation
-			mainLib = AnimationManager.MainLib,
-			steelLib = AnimationManager.libraries["steel"];
-		HudFrameData
-			hud = SifCrest.HudFrame;
-
-		hud.ProfileIcon = LoadSprite($"{path}.hud_profile.png", ppu: 100);
-		hud.SteelProfileIcon = LoadSprite($"{path}.hud_ss_profile.png", ppu: 100);
-
-		hud.Appear = mainLib.GetClipByName("Traveller HUD Appear");
-		hud.Idle = mainLib.GetClipByName("Traveller HUD Idle");
-		hud.Disappear = mainLib.GetClipByName("Traveller HUD Disappear");
-
-		hud.SteelAppear = steelLib.GetClipByName("Traveller HUD Appear");
-		hud.SteelIdle = steelLib.GetClipByName("Traveller HUD Idle");
-		hud.SteelDisappear = steelLib.GetClipByName("Traveller HUD Disappear");
-
-		hud.OnRootCreated += HudRootSetup;
-
-		void HudRootSetup() {
-			var root = hud.Root!;
-
-			var sg = root.GetOrAddComponent<SortingGroup>();
-			sg.sortingLayerName = "Over";
-			sg.sortingOrder = 0;
-
-			GameObject
-				meter = new("meter"),
-				mask = new("mask");
-
-			var meterRenderer = meter.AddComponent<SpriteRenderer>();
-			meterRenderer.sprite = LoadSprite($"{path}.hud_meter.png");
-			meterRenderer.sortingOrder = 0;
-			meterRenderer.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
-			meter.transform.SetParent(root.transform);
-			meter.layer = (int)PhysLayers.UI;
-
-			var maskRenderer = mask.AddComponent<SpriteMask>();
-			maskRenderer.sprite = LoadSprite($"{path}.hud_meter_mask.png");
-			maskRenderer.sortingOrder = 1;
-			mask.transform.SetParent(root.transform);
-			mask.transform.localScale = Vector3.zero;
-			mask.layer = (int)PhysLayers.UI;
-
-			HeroController.instance.InvokeNextFrame(() => {
-				root.transform.parent.localScale = Vector3.one;
-				root.transform.parent.localPosition = Vector2.zero;
-
-				var activator = root.transform.parent.gameObject.GetOrAddComponent<HudRootActivator>();
-				activator.hudroot = root;
-				activator.crest = SifCrest;
-
-				// critical for layering the hud frame, meter, and full-silk-orb correctly
-				float bindOrbZ = root.transform.parent.parent.position.z;
-				root.transform.localPosition = new(-1.54f, 0.15f);
-				Vector3 rootPos = root.transform.position;
-				root.transform.position = new(rootPos.x, rootPos.y, bindOrbZ - 0.00001f);
-
-				root.transform.localScale = Vector3.one;
-				meter.transform.localPosition = Vector3.zero;
-				mask.transform.localPosition = Vector3.zero;
-			});
-		}
-
-		hud.Coroutine = HudCoro;
-
-		IEnumerator HudCoro(BindOrbHudFrame hudInstance) {
-			HeroController hc = HeroController.instance;
-			PlayerData pd = PlayerData.instance;
-			GameObject mask = hud.Root!.FindChild("mask")!, glow = hud.Root!.FindChild("glow")!;
-			while(true) {
-				if (hc.IsPaused()) {
-					yield return null;
-					continue;
-				}
-				int masksMissing = pd.maxHealth - pd.health;
-				if (masksMissing == 9) {
-					if (mask.transform.localScale != Vector3.one) {
-						hudInstance.hunterV2ChargedAudio.SpawnAndPlayOneShot(GlobalSettings.Audio.DefaultUIAudioSourcePrefab, hudInstance.transform.position);
-					}
-					mask.transform.localScale = Vector3.one;
-				}
-				else {
-					float scale = masksMissing / 10f;
-					mask.transform.localScale = new(scale, scale);
-				}
-				yield return null;
-			}
-		}
-
-		#endregion
-
 		SifCrest.BindEvent = Bind.OnBindStart;
 
+		HUD.Setup();
 		Moveset.Setup();
 
 		Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
