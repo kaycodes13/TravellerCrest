9f1a47ba557e185a089765fe9f91192e6e45e74a
kaycodes13 <egad.coding@gmail.com>
2026-03-13 15:41:40 -0400

    overhaul downslash behaviour

---
+++ Attacks/DownAttackTravelling.cs
@@ -0,0 +1,10 @@
+﻿namespace TravellerCrest.Attacks;
+
+internal class DownAttackTravelling : DownAttackPositionable {
+	public AttackTravelProxy? Travel { get; set; }
+
+	protected override void LateInitializeComponents(HeroController hc) {
+		base.LateInitializeComponents(hc);
+		Travel?.Initialize(GameObject!);
+	}
+}
